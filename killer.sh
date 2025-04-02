f367ba6cadef7555251c3d904aaaf7d53bba889b
hackerlove123 <149338972+hackerlove123@users.noreply.github.com>
2025-04-02 23:29:32 +0700

    Update killer.sh

--- killer.sh
+++ killer.sh
@@ -15,7 +15,7 @@ tep_tam=$(mktemp)
 tong=0
 
 # Lấy proxy từ các loại HTTP, HTTPS, SOCKS4, SOCKS5
-for loai in socks4 socks5; do 
+for loai in http; do 
   lien_ket="https://raw.githubusercontent.com/neganok/NGCSLPRX/refs/heads/main/Proxies/$loai.txt"
   
   # Tải về và xử lý định dạng (đảm bảo mỗi proxy 1 dòng)
